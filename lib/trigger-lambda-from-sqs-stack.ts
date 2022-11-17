22c820723738434512b2b1a189838f2474b99e91
Rabi Gautam <rabi@sharelookapp.com>
2022-11-17 15:10:23 +0545

    feat:add queue and lambda to be invoke

--- lib/trigger-lambda-from-sqs-stack.ts
+++ lib/trigger-lambda-from-sqs-stack.ts
@@ -1,7 +1,7 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as sqs from "aws-cdk-lib/aws-sqs";
+import * as lambda from "@aws-cdk/aws-lambda";
 export class TriggerLambdaFromSqsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -12,8 +12,15 @@ export class TriggerLambdaFromSqsStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'TriggerLambdaFromSqsQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
-    const queue = new sqs.Queue(this,'TriggerSQSQueue', {
-        queueName:'TestSQSQueue',
-    })
+    const queue = new sqs.Queue(this, "TriggerSQSQueue", {
+      queueName: "TestSQSQueue"
+    });
+    const lambdaFunction = new lambda.Function(this, "Function", {
+      code: lambda.Code.fromAsset("src"),
+      handler: "index.handler",
+      functionName: "SqsMessageHandler",
+      runtime: lambda.Runtime.NODEJS_12_X
+    });
+    
   }
 }
