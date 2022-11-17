9b6289a40a560eeeb9cf790c4099789e3f0131a2
Rabi Gautam <rabi@sharelookapp.com>
2022-11-17 11:20:40 +0545

    feat:add sqs as IaaC

--- lib/trigger-lambda-from-sqs-stack.ts
+++ lib/trigger-lambda-from-sqs-stack.ts
@@ -1,6 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class TriggerLambdaFromSqsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,5 +12,8 @@ export class TriggerLambdaFromSqsStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'TriggerLambdaFromSqsQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+    const queue = new sqs.Queue(this,'TriggerSQSQueue', {
+        queueName:'TestSQSQueue',
+    })
   }
 }
